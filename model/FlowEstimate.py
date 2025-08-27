25bfbe9fc7938c939b36a73b7ad42d8ea9deeace
Sheng <62434964+shermo1415@users.noreply.github.com>
2025-08-27 20:16:01 +0800

    Update FlowEstimate.py

--- model/FlowEstimate.py
+++ model/FlowEstimate.py
@@ -1,3 +1,8 @@
+'''
+Source code for the time-continuous manifold sampling branch
+in:  B * Cin * H * W                        #Img
+out: B * (4*emb_dim) * (H/2) * (W/2)        #F_Img
+'''
 import torch
 import torch.nn as nn
 import numpy as np
@@ -159,4 +164,5 @@ if __name__ == '__main__':
     timestep = 0.5
 
     c = n(img0, img1, ens, fls, timestep)
+
 
